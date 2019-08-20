a368bb206ac3f123477e82d71919acdc0772c5fb
Tommy Vinyard <tpvinyard@gmail.com>
2019-08-19 21:45:49 -0500

    adding recognition of crrect and incorrect answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,8 +19,9 @@ $(document).ready(function() {
 
     const game = {
         correctAnswer: [2,1],
-        questionIterator: 1,
-        answerIterator: 0,
+        questionIterator: 0,
+        numberOfCorrectAnswers: 0,
+        numberOfIncorrectAnswers: 0,
 
         questionTimer: function() {
             intervalId = setInterval(game.countDown, 1000);
@@ -33,20 +34,26 @@ $(document).ready(function() {
         },
 
         showQuestion: function() {
+            $('.questionBlock').show();
             $('#question').text(questions[Object.keys(questions)[game.questionIterator]]);
             for (let i=0; i < answers.answers1.length; i++) {
-                $('#question').append(`<button type="button" class="btn">${answers[Object.keys(answers)[game.answerIterator]][i]}</button>`);
-
+                $('#answer').append(`<div><button type="button" class="btn btn-primary answers">${answers[Object.keys(answers)[game.questionIterator]][i]}</button></div>`);
             }
+            questionIterator++;
+        },
+
+        showCorrect: function() {
 
         },
 
-        isAnswerCorrect: function() {
+        showIncorrect: function() {
 
         },
 
         reset: function() {
             timeLeft = 31;
+            game.numberOfCorrectAnswers = 0;
+            game.numberOfIncorrectAnswers = 0;
         }
     }
 
@@ -54,6 +61,23 @@ $(document).ready(function() {
         game.questionTimer();
         game.showQuestion();
         $('#start-button').hide();
+    });
+
+    $('.questionBlock').on('click', 'button.answers',function() {
+        console.log($(this).text())
+        //if answer is correct
+        if ($(this).text() == answers[Object.keys(answers)[game.questionIterator]][game.correctAnswer[game.questionIterator]]) {
+            game.showCorrect();
+            game.numberOfCorrectAnswers++;
+            setTimeout(game.showQuestion(),5000);
+        } 
+
+        //if answer is incorrect
+        else {
+            game.showIncorrect();
+            game.numberOfIncorrectAnswers++
+            setTimeout(game.showQuestion(),5000);
+        }
     });
 
 });
